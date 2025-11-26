5e6b1cab3f33317c1db8d750b9a9ab0dd5e11eba
Ayazlatif35 <ayazlatif35@gmail.com>
2025-11-26 16:50:23 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,6 @@ import streamlit_authenticator as stauth
 st.set_page_config(page_title="Secure Name Search", layout="centered")
 
 
-
-name, auth_status, username = authenticator.login("Login", "main")
-
-if auth_status is False:
-    st.error("Incorrect username or password")
-elif auth_status is None:
-    st.warning("Please enter your username and password")
-
-if auth_status:
-    authenticator.logout("Logout", "sidebar")
-    st.sidebar.write(f"Logged in as: {name}")
-
     st.title("🔍 Secure Name Search App")
 
     df = pd.read_excel("sample_data.xlsx")
